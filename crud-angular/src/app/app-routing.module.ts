af324e100bb6166a001e3e5ccebef0e58af9f25c
Lucas <lucascagostinho@gmail.com>
2026-04-01 23:22:20 -0300

    Alterado a versao do projeto angular e do node

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'courses' },
-  { path: 'courses', loadChildren: () => import('./courses/courses-module').then(m => m.CoursesModule) }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- crud-angular/src/main.ts
+++ crud-angular/src/main.ts
@@ -1,7 +1,12 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModule } from './app/app-module';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-platformBrowser().bootstrapModule(AppModule, {
-  
-})
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
